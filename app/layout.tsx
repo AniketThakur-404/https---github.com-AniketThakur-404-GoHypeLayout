c2b6beda7cb2b332a6db6dc9a729eceda1e85011
AniketThakur-404 <anikett331@gmail.com>
2025-10-26 23:03:46 +0530

    seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,18 @@
 import type { Metadata } from 'next'
+import Script from 'next/script'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
-import { Analytics } from '@vercel/analytics/next'
+import { Analytics } from '@vercel/analytics/react'
 import './globals.css'
 import CursorSpotlight from '../components/cursor-spotlight'
-
+import GtagPageView from './gtag-pageview'
 export const metadata: Metadata = {
   title: 'Gohype',
   description: 'Created by Gohype',
   generator: '',
 }
+
+const GA_ID = process.env.NEXT_PUBLIC_GOOGLE_ADS_ID
 
 export default function RootLayout({
   children,
@@ -18,7 +21,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
+      <body className={`${GeistSans.variable} ${GeistMono.variable}`}>
+        
+        // ...
+        <GtagPageView />
+
+        {/* Google tag (gtag.js) – loads after hydration */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_ID}');
+          `}
+        </Script>
+
         <CursorSpotlight />
         {children}
         <Analytics />
